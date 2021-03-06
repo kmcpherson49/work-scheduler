bd64d34a35349bbc9037f6f92564fd1577322457
wbwd <77216609+kmcpherson49@users.noreply.github.com>
2021-03-05 20:00:40 -0700

    end of input error

--- script.js
+++ script.js
@@ -1,48 +1,33 @@
-$(document).ready(function() {
-    $(".rowBtn").on("click", function(){
-        var inputValue = $(this).parent().find("input").val();
-        var inputTime = $(this).parent().attr("id");
-        localStorage.setItem(inputValue, inputTime);
+var inputValue = {};
+var inputTime = {};
+
+$(document).ready(function () {
+  $(".saveBtn").on("click", function () {
+    inputValue = $(this).parent().find("input").val();
+    inputTime = $(this).parent().attr("id");
+    localStorage.setItem(inputValue, inputTime);
+
     console.log(inputValue);
     console.log(inputTime);
-    })
+  });
 
-    var currentDate = moment().format("MM/DD/YYYY");
-    $("#theDate").append(currentDate);
-    
+  var currentDate = moment().format("dddd, MMMM Do YYYY");
+  $("#theDate").append(currentDate);
 
+    //retireve localstorage
+    localStorage.getItem(
+    "inputValue",
+    "inputTime"
+  );
 
-
-
-
-
-
-
-
-
-
+  var loadTasks = function () {
+    inputTime = localStorage.getItem(inputTime);
+    inputValue = localStorage.getItem(inputValue);
 
 
 
 
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-})
+// load tasks for the first time
+loadTasks();
